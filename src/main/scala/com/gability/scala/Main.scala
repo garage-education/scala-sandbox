aee11c44173e74ea8c5b69d2d33c30900d735a53
moustafaalaa <mustafaalaa.mohamed@gmail.com>
2020-05-09 06:05:36 +0200

    update scaladoc

--- src/main/scala/com/gability/scala/Main.scala
+++ src/main/scala/com/gability/scala/Main.scala
@@ -1,12 +1,30 @@
 package com.gability.scala
 
+/**
+ * The main object for our application.
+ */
 object Main extends App {
-  def workCount(s:String): Int = {
-    s match {
+
+  /**
+   * This function counts the words in a given string.
+   *
+   * @param str represents the input string we are going t count its words.
+   * @return integer representing the word count in the input string.
+   */
+  def workCount(str: String): Int = {
+    str match {
       case str if isEmpty(str) => 0
-      case str => str.split("\\W+").length
+      case x => x.split("\\W+").length
     }
   }
-  def isEmpty(x: String) = x == null || x.trim.isEmpty
+
+  /**
+   * This function checks if the input string is not null or empty.
+   *
+   * @param str represents the input string we are going to check.
+   * @return true if the input string is null or empty otherwise returns false.
+   */
+  def isEmpty(str: String) = str == null || str.trim.isEmpty
+
   println(workCount("Moustafa Alaa"))
 }
